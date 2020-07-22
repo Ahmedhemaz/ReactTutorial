0d346ac5635f874f307eff4f44a8e2c3c18b354c
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-07-22 21:48:21 +0200

    App and Person with class Component style

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React, { useState } from 'react';
+import React, { Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
-
-const App = props =>{
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       {name: 'ahmed', age:26},
       {name: 'shaf3y', age:27},
       {name: 'ziad', age:28}
     ]
-  });
-  const submitBtnHandler = () =>{
-    setPersonsState({
+  }
+  submitBtnHandler = () =>{
+    this.setState({
       persons: [
         {name: 'ahmed', age:27},
         {name: 'shaf3y', age:28},
         {name: 'ziad', age:29}
     ]})
   }
+  render() {
+
     return (
       <div className="App">
         <h1>Hello React</h1>
-        <button onClick={submitBtnHandler}>Submit</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}></Person>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
+        <button onClick={this.submitBtnHandler}>Submit</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
+  }
 }
+
 export default App;
