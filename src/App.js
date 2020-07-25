84e002488ad6d9704f89f09e6a2509e53c58c93d
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-07-25 15:15:02 +0200

    RaduimAndStylesWithDynamicWay

--- src/App.js
+++ src/App.js
@@ -1,34 +1,100 @@
 import React, { Component} from 'react';
 import './App.css';
+import Raduim, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      {name: 'ahmed', age:26},
-      {name: 'shaf3y', age:27},
-      {name: 'ziad', age:28}
-    ]
+      {id: 'qwdoasflkaf_1',name: 'ahmed', age:26},
+      {id: 'sfohasfasf_2' ,name: 'shaf3y', age:27},
+      {id: 'asdoqwijdos_3',name: 'ziad', age:28}
+    ],
+    isPersonsShown: true
   }
-  submitBtnHandler = () =>{
+  submitBtnHandler = (newName) =>{
     this.setState({
       persons: [
-        {name: 'ahmed', age:27},
+        {name: newName, age:27},
         {name: 'shaf3y', age:28},
         {name: 'ziad', age:29}
     ]})
   }
+  nameChangedHandler = (event, personId)=>{
+    const personIndex = this.state.persons.findIndex(person => person.id === personId );
+    const personCopy = {...this.state.persons[personIndex]};
+    personCopy.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = personCopy;
+    this.setState({persons:persons})
+  }
+
+  toggelPersonHandler = ()=>{
+    const isPersonHiddenFlag = this.state.isPersonsShown;
+    this.setState({isPersonsShown: !isPersonHiddenFlag})
+  }
+
+  deletePersonHandler = (personIndex)=>{
+    const personsTemp = [...this.state.persons]
+    personsTemp.splice(personIndex,1);
+    this.setState({persons: personsTemp});
+  }
   render() {
 
+    const style = {
+      backgroundColor : 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1x solid blue',
+      padding: '8x',
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+
+    }
+    let persons = null;
+    if(this.state.isPersonsShown) {
+      persons = (
+        <div>
+          {this.state.persons.map((person,i) => {
+            return <Person 
+            key={person.id}
+            name={person.name} 
+            age={person.age}
+            click ={this.deletePersonHandler.bind(this, i)}
+            nameChanged={(event)=>this.nameChangedHandler(event, person.id)}/>
+          })}
+        </div>
+      )
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'lightred',
+        color: 'black'
+      }
+    }
+
+    const classess = [];
+    if(this.state.persons.length <=2){
+      classess.push('red');
+    }
+    if(this.state.persons.length <=1){
+      classess.push('bold');
+    }
+  
     return (
-      <div className="App">
-        <h1>Hello React</h1>
-        <button onClick={this.submitBtnHandler}>Submit</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hello React</h1>
+          <p className={classess.join(' ')}>This is really working</p>
+          <button 
+          style={style}
+          onClick={this.toggelPersonHandler}>TogglePersons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Raduim(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Person.css';
+import Raduim from 'radium';
 const Person = (props)=>{
+    const style = {
+        '@media (max-width: 500px':{
+            width: '400px'
+        }
+    }
     return (
-            <div>
-                <p>My name is {props.name} and i'm {props.age} years old </p>
+            <div className='Person' style={style}>
+                <p onClick={props.click}>My name is {props.name} and i'm {props.age} years old </p>
                 <p>{props.children}</p>
+                <input type='text' onChange={props.nameChanged} value={props.name}/>
             </div>
         );
 }
-export default Person
+export default Raduim(Person);
