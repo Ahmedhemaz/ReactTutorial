1d0015c6e7e0d013c3e212b6f87cdd4bd16130bf
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-08-25 05:28:37 +0200

    Get 1 post and post new post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,6 +8,7 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
+        selectedPostId: null
     }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -23,9 +24,17 @@ class Blog extends Component {
         });
         
     }
+
+    selectPostHandler(id){
+        this.setState({selectedPostId: id});
+    }
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
+            return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={()=> this.selectPostHandler(post.id)}/>
         }) 
         return (
             <div>
@@ -33,7 +42,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost  id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
